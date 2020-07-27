d517d81d3a2e8ac079c6a15beb7e8564d2aeb911
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2020-07-27 17:59:32 +0530

    Create Lecture50,Arrays.js

---
+++ Javascript/Lecture50,Arrays.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Arrays in Javascript</title>
+        <script src="script50.js"></script>
+    </head>
+    <body>
+        <h1>Arrays in JS.</h1>
+    </body>
+</html>
